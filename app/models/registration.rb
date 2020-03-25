24793dd005dd38b1d061d24002f983f2be42c35a
mrarthurwhite <mister.arthur.white@gmail.com>
2020-03-25 14:11:36 -0400

    made rsvp compulsory in registration model; deleted idea code commented out earlier

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,16 +2,7 @@ class Registration < ApplicationRecord
   belongs_to :user
   belongs_to :event
   belongs_to :registrant, class_name: "User", foreign_key:"user_id"
-  validates :rsvp, inclusion: { in: %w(Yes No Maybe)}
-
-=begin
-  def self.rsvp_options
-    yes = { :rsvp=>"Yes"}
-    no = { :rsvp=>"No"}
-    maybe = { :rsvp=>"Maybe"}
-    return [yes, no, maybe]
-  end
-=end
+  validates :rsvp, presence: true, inclusion: { in: %w(Yes No Maybe)}
 
 
 end
